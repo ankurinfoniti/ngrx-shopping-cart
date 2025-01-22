750d3afc2148b0bce3057e7d35f8fe623542ba36
ankurinfoniti <ankur.infoniti@gmail.com>
2025-01-22 19:42:15 +0530

    feat: add navigation bar and routing for shop, cart, and counter components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './core/components/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'Ngrx Shopping Cart';
-}
+export class AppComponent {}
---
+++ src/app/features/counter/counter.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  imports: [],
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.css',
+})
+export default class CounterComponent {}
